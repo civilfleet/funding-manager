ee94ee7b3a9a3daee59f5c92819130e66a53eb6d
ahsan131-hub <Muhammad.Ahsan@b-tu.de>
2025-03-03 03:48:05 +0100

    fix: update import paths for Loader component in file uploader and button control

--- components/helper/button-control.tsx
+++ components/helper/button-control.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Loader } from "@components/helper/loader";
+import { Loader } from "./loader";
 
 export default function ButtonControl({
   type = "submit",
