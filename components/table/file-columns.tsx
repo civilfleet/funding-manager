2320fc9df88df38817264b2506fd0fa7e54cbcca
ahsan131-hub <Muhammad.Ahsan@b-tu.de>
2025-03-20 08:25:09 +0100

    refactor: remove unused action column and related dropdown menu from file columns

--- components/table/file-columns.tsx
+++ components/table/file-columns.tsx
@@ -1,39 +1,7 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { File } from "@/types";
 import Link from "next/link";
 export const columns: ColumnDef<File>[] = [
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const request = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="start">
-            <a href={`funding-request/${request.id}`}>
-              <DropdownMenuItem>View</DropdownMenuItem>
-            </a>
-            <DropdownMenuSeparator />
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
   {
     accessorKey: "organization",
     header: () => <div className="text-left w-36">Organization</div>,
