84dfb7794c9b6117fbb6026ce5239e399f873396
Qiong Wu <qiong.wu@gfnork.de>
2025-05-14 11:54:12 +0200

    fix auto redirect on login

--- app/organizations/page.tsx
+++ app/organizations/page.tsx
@@ -1,4 +1,28 @@
-export default function Page() {
+import { redirect } from "next/navigation";
+import { Roles } from "@/types";
+import { getUserCurrent } from "@/services/users";
+import { auth } from "@/auth";
+
+export default async function Page() {
+  const session = await auth();
+
+  if (session?.user?.userId) {
+    if (session.user.roles?.includes(Roles.Admin)) {
+      return redirect("/admin");
+    } else if (session.user.roles?.includes(Roles.Organization)) {
+      const userData = await getUserCurrent(session.user.userId);
+      if (userData?.organizations && userData.organizations.length > 0) {
+        return redirect(`/organizations/${userData.organizations[0].id}/profile`);
+      }
+      return redirect("/organizations");
+    } else if (session.user.roles?.includes(Roles.Team)) {
+      const userData = await getUserCurrent(session.user.userId);
+      if (userData?.teams && userData.teams.length > 0) {
+        return redirect(`/teams/${userData.teams[0].id}/organizations`);
+      }
+      return redirect("/teams");
+    }
+  }
   return (
     <div className="px-3 py-2 text-sm text-muted-foreground">
       Please select a team or organization from the sidebar.
