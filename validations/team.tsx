e801b1df1d11bcc32e39696a2bcaa0a793822775
Qiong Wu <qiong.wu@gfnork.de>
2025-07-22 19:14:35 +0200

    make phone and address optional for contact person

--- validations/team.tsx
+++ validations/team.tsx
@@ -18,8 +18,8 @@ const createTeamSchema = z.object({
   user: z.object({
     name: z.string().min(1, "Name is required"),
     email: z.string().email("Invalid email address"),
-    phone: z.string().min(1, "Phone is required"),
-    address: z.string().min(1, "Address is required"),
+    phone: z.string().optional(),
+    address: z.string().optional(),
   }),
 });
 
