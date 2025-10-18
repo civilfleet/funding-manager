6c1dc2b8d9c530ad76c75c80d5ba91c87ee29b1f
Qiong Wu <qiong.wu@gfnork.de>
2025-10-18 03:46:52 +0200

    fix instrumentation

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,3 +1,4 @@
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
 // This file configures the initialization of Sentry on the client.
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
