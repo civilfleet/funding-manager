49a5727b79dacbe780f4202e4c997742e01639f5
Qiong Wu <qiong.wu@gfnork.de>
2025-11-03 21:51:09 +0100

    remove deprecated property

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     serverActions: {
       allowedOrigins: [
